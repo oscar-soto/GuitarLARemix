0850d56c483043b3e678e0b5ebf74e2fd6282900
Oscar <oscarantonio.soto1998@gmail.com>
2023-01-27 18:35:00 -0300

    Fix: add logo to header and css styles

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,29 +1,22 @@
-import { Link } from "@remix-run/react"
+import { Link } from '@remix-run/react';
+import logo from '../../public/img/logo.svg';
 
 const Header = () => {
   return (
     <header className="header">
       <div className="container bar">
-        <div className="logo">
-
-        </div>
+        <Link to="/" className="logo">
+          <img className="logo" src={logo} alt="imagen del logo" />
+        </Link>
         <nav className="navigation">
-          <Link
-            to="/"
-          >Inicio</Link>
-          <Link
-            to="/nosotros"
-          >Nosotros</Link>
-          <Link
-            to="/tienda"
-          >Tienda</Link>
-          <Link
-            to="/blog"
-          >Blog</Link>
+          <Link to="/">Inicio</Link>
+          <Link to="/nosotros">Nosotros</Link>
+          <Link to="/tienda">Tienda</Link>
+          <Link to="/blog">Blog</Link>
         </nav>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
