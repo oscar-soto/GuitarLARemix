1064874821eb1b66ccb40ae223544c510a698916
Oscar <oscarantonio.soto1998@gmail.com>
2023-01-29 19:41:00 -0300

    Fix: formatted date

--- app/components/post.jsx
+++ app/components/post.jsx
@@ -1,4 +1,5 @@
 import { Link } from '@remix-run/react';
+import { formatDate } from '~/utils/helpers';
 
 const Post = ({ post }) => {
   const { content, publishedAt, title, url, imagen } = post;
@@ -13,7 +14,7 @@ const Post = ({ post }) => {
       </figure>
       <div className="content">
         <h3>{title}</h3>
-        <p className="date">{publishedAt}</p>
+        <p className="date">{formatDate(publishedAt)}</p>
         <p className="summary">{content}</p>
         <Link className='link' to={`/posts/${url}`}>Leer entradas</Link>
       </div>
