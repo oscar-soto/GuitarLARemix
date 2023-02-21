819b9223007bc99372183de974a394153ed85415
Oscar <oscarantonio.soto1998@gmail.com>
2023-02-20 22:33:08 -0300

    Fix: hydrated component in Remix

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,5 +1,6 @@
 import { useOutletContext } from '@remix-run/react';
 import { useEffect, useState } from 'react';
+import { ClientOnly } from 'remix-utils';
 import styles from '~/styles/cart.css';
 
 export const meta = () => {
@@ -33,70 +34,74 @@ const Cart = () => {
   }, [cart]);
 
   return (
-    <main className="container">
-      <h1 className="heading">Carrito de Compras</h1>
+    <ClientOnly fallback={'Cargando...'}>
+      {() => (
+        <main className="container">
+          <h1 className="heading">Carrito de Compras</h1>
 
-      <div className="content">
-        <div className="cart">
-          <h2>Articulos</h2>
+          <div className="content">
+            <div className="cart">
+              <h2>Articulos</h2>
 
-          {cart?.length > 0 &&
-            cart?.map((product) => (
-              <div key={product.id} className="product">
-                <div>
-                  <img
-                    src={product.image}
-                    alt={`Imagen del producto ${product.name}`}
-                  />
-                </div>
+              {cart?.length > 0 &&
+                cart?.map((product) => (
+                  <div key={product.id} className="product">
+                    <div>
+                      <img
+                        src={product.image}
+                        alt={`Imagen del producto ${product.name}`}
+                      />
+                    </div>
 
-                <div>
-                  <p className="name">{product.name}</p>
-                  <p>Cantidad</p>
+                    <div>
+                      <p className="name">{product.name}</p>
+                      <p>Cantidad</p>
 
-                  <select
-                    value={product.amount}
-                    className="select"
-                    onChange={(e) =>
-                      updateAmount({
-                        amount: +e.target.value,
-                        id: product.id,
-                      })
-                    }
-                  >
-                    <option value="0">-- Seleccione --</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                  </select>
+                      <select
+                        value={product.amount}
+                        className="select"
+                        onChange={(e) =>
+                          updateAmount({
+                            amount: +e.target.value,
+                            id: product.id,
+                          })
+                        }
+                      >
+                        <option value="0">-- Seleccione --</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                      </select>
 
-                  <p className="price">
-                    $ <span>{product.price}</span>
-                  </p>
-                  <p className="subtotal">
-                    Subtotal$ <span>{product.amount * product.price}</span>
-                  </p>
-                </div>
+                      <p className="price">
+                        $ <span>{product.price}</span>
+                      </p>
+                      <p className="subtotal">
+                        Subtotal$ <span>{product.amount * product.price}</span>
+                      </p>
+                    </div>
 
-                <button
-                  type="button"
-                  className="btn_delete"
-                  onClick={() => deleteGuitar(product.id)}
-                >
-                  x
-                </button>
-              </div>
-            ))}
-        </div>
+                    <button
+                      type="button"
+                      className="btn_delete"
+                      onClick={() => deleteGuitar(product.id)}
+                    >
+                      x
+                    </button>
+                  </div>
+                ))}
+            </div>
 
-        <aside className="summary">
-          <h3>Resumen del Pedido</h3>
-          <p>Total de pagar: ${total}</p>
-        </aside>
-      </div>
-    </main>
+            <aside className="summary">
+              <h3>Resumen del Pedido</h3>
+              <p>Total de pagar: ${total}</p>
+            </aside>
+          </div>
+        </main>
+      )}
+    </ClientOnly>
   );
 };
 
